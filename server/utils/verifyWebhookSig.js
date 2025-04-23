a44c7c3acbe7b4ba5a5fa1abe0fbc7821a9b4afc
Ify Nweze <Ify.Nweze@checkout.com>
2025-04-23 17:54:38 +0100

    CKO-SIG fix

--- server/utils/verifyWebhookSig.js
+++ server/utils/verifyWebhookSig.js
@@ -1,19 +1,23 @@
 const crypto = require('crypto');
+const getParameter = require('./utils/getAWSParameter');
 
-// HMAC sig verfication 
+let CKO_WEBHOOK_SIGNATURE = null;
 
 /**
  * Verifies the HMAC signature to ensure the webhook's integrity.
  * @param {Buffer} rawBody - The raw body of the webhook request.
  * @param {string} receivedSignature - The signature received in the webhook header.
- * @returns {boolean} - Returns true if signatures match, otherwise false.
+ * @returns {Promise<boolean>} - Resolves true if signatures match, otherwise false.
  */
+async function verifySignature(rawBody, receivedSignature) {
+    if (!CKO_WEBHOOK_SIGNATURE) {
+        CKO_WEBHOOK_SIGNATURE = await getParameter('CKO_WEBHOOK_SIGNATURE');
+    }
 
-function verifySignature(rawBody, recievedSig) {
-    const hmac = crypto.createHmac('sha256',CKO_WEBHOOK_SIGNATURE);
+    const hmac = crypto.createHmac('sha256', CKO_WEBHOOK_SIGNATURE);
     hmac.update(rawBody);
     const expectedSignature = hmac.digest('hex');
-    return expectedSignature === recievedSignature;
+    return expectedSignature === receivedSignature;
 }
 
-module.exports = verifySignature; 
+module.exports = verifySignature;
