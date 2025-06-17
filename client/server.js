2629c61af011c8233c81533e8685cb03a8b4211b
Ify Nweze <Ify.Nweze@checkout.com>
2025-06-17 15:12:40 +0100

    Apple Fix

--- client/server.js
+++ client/server.js
@@ -1,6 +1,11 @@
-const express = require('express');
-const { createProxyMiddleware } = require('http-proxy-middleware');
-const path = require('path');
+import express from 'express';
+import { createProxyMiddleware } from 'http-proxy-middleware';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Get __dirname equivalent in ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
